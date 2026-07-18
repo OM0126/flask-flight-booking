6c69823e67d7c99b8192b3402472890fd896407d
Ubuntu <ubuntu@ip-172-31-23-143.ec2.internal>
2026-07-18 17:15:29 +0000

    Add Dockerfile

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ if __name__ == "__main__":
     # create DB tables if they don't exist
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
